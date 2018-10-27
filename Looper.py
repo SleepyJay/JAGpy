0096e1a469ca126f68f61af073b89a58b4d957be
Jason Agosta <agostaj@d20green.com>
2018-10-26 22:04:52 -0400

    JPY-xxx dirty checkin

---
+++ Looper.py
@@ -0,0 +1,44 @@
+
+"""Generic looping"""
+
+
+#
+def umtha(start, end, size):
+    """b"""
+
+    full = []
+
+    for sz in range(0,size):
+        items = []
+
+        for x in range(start, end + 1):
+            items.append(x)
+
+        full.append([items])
+
+    # items = []
+    #
+    # for x in range(start,end+1):
+    #     items.append(x)
+    #
+    #     full.append(items)
+    #     pass
+
+    return full
+
+
+# [
+#   [0,0], [0,1], [0,2], [1,0]
+
+def iterate(a,b,c):
+    full = []
+
+    for x in range(0,2):
+        full.append(x)
+
+    return full
+
+
+
+
+
---
+++ run.py
@@ -0,0 +1,10 @@
+
+import Looper
+#
+items = Looper.iterate(0, 3, 4)
+
+print(items)
+        
+
+
+
