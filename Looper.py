a672fcf046a2d7b6fb629041f7e7d252342714bb
J A <agostaj@d20green.com>
2018-10-28 22:22:51 -0400

    JPY-xxx some looper fixes

--- Looper.py
+++ Looper.py
@@ -3,42 +3,43 @@
 
 
 #
-def umtha(start, end, size):
-    """b"""
-
+def list(start,stop,size):
     full = []
-
-    for sz in range(0,size):
-        items = []
-
-        for x in range(start, end + 1):
-            items.append(x)
-
-        full.append([items])
-
-    # items = []
-    #
-    # for x in range(start,end+1):
-    #     items.append(x)
-    #
-    #     full.append(items)
-    #     pass
-
+        
+    for x in range(start, stop):
+        full.append([x])
+    
+    nxt = []
+    for i in range(0, size-1):
+        nxt = []
+        for z in range(start, stop):
+            for l in full: 
+                cur = []   
+                for y in l:
+                    cur.append(y)
+                print(f"c>{cur}")
+                cur.append(z)
+                nxt.append(cur)
+    
+        full = nxt
+        #print(f"f>{full}")
     return full
 
-
-# [
-#   [0,0], [0,1], [0,2], [1,0]
-
-def iterate(a,b,c):
-    full = []
-
-    for x in range(0,2):
-        full.append(x)
-
-    return full
-
-
-
-
-
+# turn a number in to a list of bits with a given base.    
+# options:
+    # start, stop, value: the list is normalized up to the range given (# needs better words)
+    # stop, value: go from 0 to base in the "bits" 
+def value_to_list(start, stop, value):
+    list = []
+    
+    base = stop-start
+    iter = value
+    while iter > base:
+         res = divmod(iter, base)
+         iter = res[0]
+         list.append(res[1])
+    
+    list.append(iter)        
+    
+    return list
+        
--- run.py
+++ run.py
@@ -1,10 +1,46 @@
 
-import Looper
-#
-items = Looper.iterate(0, 3, 4)
+#import Looper
 
-print(items)
-        
+import imp; imp.reload(Looper)
 
+
+# only does 0-1 x 3 (for reference)
+def boring():
+    boring = []
+    for x in range(0, 2):
+        for y in range(0, 2):
+            for z in range(0,2):
+                boring.append([x,y,z])    
+
+    return boring
+
+# ----
+
+boring = boring()
+i=0 
+for p in boring:
+    i += 1
+    print(f"{i}: {p}")
+
+print('----')
+
+# ----
+
+items = Looper.list(0, 2, 3)
+i=0
+for p in items:
+    i += 1
+    print(f"{i}: {p}")
+
+# ----
+
+itered = Looper.value_to_list(0,5,87)
+print(f"{itered}")
+#i=0 
+#for p in boring:
+#    i += 1
+#    print(f"{i}: {p}")
+
+print('----')        
 
 
