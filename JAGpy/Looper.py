7ed773f7aae440936c88cb281dc7b5714722b27b
Jason Agosta <agostaj@d20green.com>
2019-06-20 22:53:57 -0400

    JPY-xxx fleshed out Looper

--- JAGpy/Looper.py
+++ JAGpy/Looper.py
@@ -1,6 +1,7 @@
 
 """Generic looping"""
 
+from collections import deque
 
 #
 def combos(start, stop, size):
@@ -66,3 +67,70 @@ def list_to_value(base, my_list):
         mul *= base
 
     return val
+
+
+def permutations_from_list(ints):
+    """Generate permutations of the given list of integers"""
+    return _do_permutate_list(ints, False)
+
+
+def permutations_from_list_rightward(ints):
+    """Generate permutations of the given list of integers; rightward tries to build bigger sums first"""
+    return _do_permutate_list(ints, True)
+
+
+def yy_do_permutate_list(ints, rightward=False):
+    queue = deque([[]])
+    cnt = len(ints)
+    while queue:
+        if rightward:
+            q = queue.pop()
+        else:
+            q = queue.popleft()
+
+        if len(q):
+            k = q[-1]
+            j = cnt - len(q)
+            m = None
+            if j in ints:
+                m = ints[j]
+            print(f"k: {k}, j: {j}, m: {m}")
+
+        else:
+            k = 0
+
+        for i in range(k, cnt):
+            print(f"i: {i}")
+            x = ints[i]
+            cur = []
+            cur.extend(q)
+            cur.append(x)
+
+            if len(cur) < cnt:
+                queue.append(cur)
+
+            yield cur
+
+
+def _do_permutate_list(ints, rightward=False):
+    queue = deque([[]])
+    cnt = len(ints)
+    while queue:
+        if rightward:
+            q = queue.pop()
+        else:
+            q = queue.popleft()
+
+        if len(q):
+            k = q[-1]+1
+        else:
+            k = 0
+
+        for i in range(k, cnt):
+            cur = list(q)
+            cur.append(i)
+
+            if len(cur) < cnt:
+                queue.append(cur)
+
+            yield list(map(lambda y: ints[y], cur))
